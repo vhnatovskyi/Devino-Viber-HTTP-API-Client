4322def068e55b965a68026544248cad2159cf05
Vladislav Gnatovskyi <gnatovskyi@gmail.com>
2019-08-15 10:59:40 +0300

    Add throw exeption if Bad creditials

---
+++ Devino.API/Models/Viber/ErrorResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace Devino.API.Models.Viber
+{
+    [DataContract]
+    public class ErrorResult
+    {
+        [DataMember]
+        public string Path { get; set; }
+        [DataMember]
+        public string Error { get; set; }
+        [DataMember]
+        public string Status { get; set; }
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public long TimeStamp { get; set; }
+    }
+}
---
+++ Devino.API/Models/Viber/ViberApiException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Devino.API.Models.Viber
+{
+    public class ViberApiException:Exception
+    {
+        public ErrorResult Error { get; set; }
+        public ViberApiException(ErrorResult error) : base(error.Message)
+        {
+            Error = error;
+        }
+    }
+}
